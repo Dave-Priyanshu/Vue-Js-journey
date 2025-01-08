02483b4cd6f376190bbc77940f79facc880369cd
Dave-Priyanshu <davepriyanshu2001@gmail.com>
2025-01-08 18:44:27 +0530

    Learned Attribute binding

--- Vue-Firebase/js/list.js
+++ Vue-Firebase/js/list.js
@@ -3,11 +3,11 @@ const app = Vue.createApp({
         return{
             showBooks : true,
             books : [
-                { title: "The Guide", author: "R.K. Narayan", age: 98 },
-                { title: "God of Small Things", author: "Arundhati Roy", age: 62 },
-                { title: "Train to Pakistan", author: "Khushwant Singh", age: 99 },
-                { title: "A Suitable Boy", author: "Vikram Seth", age: 72 },
-                { title: "The White Tiger", author: "Aravind Adiga", age: 49 }
+                { title: "The Guide", author: "R.K. Narayan", age: 98 , img: "../assets/boy.png"},
+                { title: "God of Small Things", author: "Arundhati Roy", age: 62 ,img: "../assets/employee.png"},
+                { title: "Train to Pakistan", author: "Khushwant Singh", age: 99 ,img: "../assets/man.png"},
+                { title: "A Suitable Boy", author: "Vikram Seth", age: 72 ,img: "../assets/girl.png"},
+                { title: "The White Tiger", author: "Aravind Adiga", age: 49 ,img: "../assets/woman.png"}
             ]
         }
     },
